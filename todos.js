bcf24a547630b5478291a323b21f6efb3fa1cf06
HeiddisAnna <heiddisanna92@gmail.com>
2019-02-26 14:11:52 +0000

    completed komið

--- todos.js
+++ todos.js
@@ -2,21 +2,18 @@ const { query } = require('./db');
 
 
 async function getList(completed, order = 'ASC') {
-
   const orderString = order.toLowerCase() === 'desc' ? 'DESC' : 'ASC';
 
-  if(completed === 'false' || completed === 'true') {
-    
+  if (completed === 'false' || completed === 'true') {
+    const completedQ = `SELECT * FROM assignment WHERE completed = $1 ORDER BY position ${ orderString }`;
+    const completedResult = await query(completedQ, [completed]);
+    return completedResult.rows;
   }
-  
-  console.log('order string: ' + orderString);
-  const q = `SELECT * FROM assignment ORDER BY position ${orderString}`;
 
-  
+  const q = `SELECT * FROM assignment ORDER BY position ${ orderString }`;
   const result = await query(q, []);
 
   return result.rows;
-
 }
 
 module.exports = {
